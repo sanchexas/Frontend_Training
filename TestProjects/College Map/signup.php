65de42139835114552a3b6a6fd4ecaf27a592062
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-02-27 23:55:03 +0300

    connected auth to db

--- TestProjects/College Map/signup.php
+++ TestProjects/College Map/signup.php
@@ -3,6 +3,24 @@
 
     $data = $_POST;
     if(isset($data['do_signup'])){
-        
+        $errors = array();
+        if(trim($data['login']) == ''){
+            $errors[] = 'Введите логин.';
+        }
+        if(trim($data['phone']) == ''){
+            $errors[] = 'Введите телефон.';
+        }
+        if($data['password'] == ''){
+            $errors[] = 'Введите пароль.';
+        }
+        if($data['repeat_password'] != $data['password']){
+            $errors[] = 'Повторный пароль введён неверно.';
+        }
+        if(empty($errors)){
+            //--
+        }
+        else{
+            echo '<div style="color: red;">'.array_shift($errors).'</div><hr>';
+        }
     }
 ?>
